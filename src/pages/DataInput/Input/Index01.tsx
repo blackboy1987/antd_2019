7244f9446eb99885c8362d552a272f9499ef3ded
blackboy <1169794338@qq.com>
2019-09-07 12:13:10 +0800

    Input组件

--- src/pages/DataInput/Input/Index01.tsx
+++ src/pages/DataInput/Input/Index01.tsx
@@ -1,5 +1,8 @@
 import {
   Typography,
+  Input,
+  Icon,
+  Button,
 } from 'antd';
 
 import React, { Component } from 'react';
@@ -9,6 +12,11 @@ const { Paragraph } = Typography;
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Index extends Component {
+
+  changePasswordInput = () => {
+    document.getElementById("password").setAttribute("type", "text");
+  };
+
   render() {
     return (
       <div>
@@ -16,6 +24,27 @@ class Index extends Component {
           <Paragraph>
             基本使用。
           </Paragraph>
+          <Input
+            addonAfter={<Icon type="setting" />}
+            addonBefore={<Icon type="user" />}
+            prefix="abc"
+            suffix="def"
+            onChange={(e) => console.log(e.target.value)}
+            allowClear
+          />
+
+
+          <hr />
+          <Input.TextArea
+            style={{ resize: 'none' }}
+            autosize={{ minRows: 2, maxRows: 20 }}
+          />
+          <hr />
+          <Input.Search
+            enterButton
+          />
+          <Input id="password" type="password" />
+          <Button onClick={this.changePasswordInput}>改变密码框</Button>
         </Typography>
       </div>
     );
--- src/pages/DataInput/Input/Index02.tsx
+++ src/pages/DataInput/Input/Index02.tsx
@@ -1,5 +1,7 @@
 import {
   Typography,
+  Input,
+  Tooltip,
 } from 'antd';
 
 import React, { Component } from 'react';
@@ -9,7 +11,38 @@ const { Paragraph } = Typography;
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Index extends Component {
+
+  state = {
+    cardNumber: '',
+  }
+
+  showCardNumber = (e: Event) => {
+    const value = e.target.value;
+    this.setState({
+      cardNumber: value,
+    });
+  }
+
+  formatCardNumber = (cardNumber) => {
+    // 6314 3941 2394 2134 1289 0342 31
+    let cardNumberArray = new Array([...cardNumber]);
+    cardNumberArray = cardNumberArray[0];
+    const result = cardNumberArray.map((item, index) => {
+      if (index !== 0 && index % 4 === 0) {
+        return [
+          ' ',
+          item,
+        ]
+      }
+      return item;
+    })
+
+    return <span>{new Array(result).flat(Infinity)}</span>
+  }
+
   render() {
+    const { cardNumber } = this.state;
+    const title = this.formatCardNumber(cardNumber);
     return (
       <div>
         <Typography>
@@ -17,6 +50,13 @@ class Index extends Component {
             我们为 {'<Input />'} 输入框定义了三种尺寸（大、默认、小），高度分别为 40px、32px 和 24px。
           </Paragraph>
         </Typography>
+
+        <Tooltip trigger={['focus']} title={title}>
+          <Input
+            onChange={this.showCardNumber}
+          />
+        </Tooltip>
+
       </div>
     );
   }
